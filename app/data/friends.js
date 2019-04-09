70c542f2c5e7f8c2ebfeb81e6aa82a598e4908f9
theWorthyG <wdg2001@wildcats.unh.edu>
2019-04-08 20:35:58 -0400

    first push

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+var friends = [
+	{
+		name: "Iron Man (Tony Stark)",
+		photo:"ironMan.png",
+		scores: [5, 1, 5, 4, 5, 5, 3, 5, 2, 1]
+	},
+	{
+		name: "Thor Odinson",
+		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+		scores: [1, 5, 5, 5, 5, 4, 2, 1, 5, 1]
+	},
+	{
+		name: "Spider-Man",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+		scores: [5, 5, 2, 2, 5, 2, 4, 5, 1, 1]
+	},
+	{
+		name: "Hulk (Bruce Banner)",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		scores: [5, 5, 1, 1, 4, 3, 1, 2, 1, 5]
+	},
+	{
+		name: "Deadpool (Wade Wilson)",
+		photo: "",
+		scores: [5, 3, 5, 1, 5, 2, 2, 4, 3, 1]
+    },
+    {
+		name: "Groot",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		scores: [1, 3, 3, 1, 1, 5, 5, 1, 1, 5]
+    },
+    {
+		name: "Drax",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		scores: [1, 2, 4, 1, 1, 4, 2, 1, 3, 2]
+    },
+    {
+		name: "Black Widow (Natalia Rovenoff)",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		scores: [5, 1, 4, 1, 2, 4, 3, 3, 2, 1]
+    },
+    {
+		name: "Stan Lee",
+		photo: "stanlee.png",
+		scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+	},
+    {
+		name: "Thanos",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		scores: [1, 5, 5, 3, 2, 4, 1, 1, 5, 3]
+	}
+    
+    
+
+];
+
+module.exports = friends;
