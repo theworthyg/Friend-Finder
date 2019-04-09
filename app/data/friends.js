667dd7840ff2f3cc5adba3561f713c849a734538
theWorthyG <wdg2001@wildcats.unh.edu>
2019-04-08 21:47:33 -0400

    first try

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,52 +1,52 @@
 var friends = [
 	{
 		name: "Iron Man (Tony Stark)",
-		photo:"ironMan.png",
+		photo:"https://github.com/theworthyg/Friend-Finder/blob/master/app/friendpictures/ironMan.jpg?raw=true",
 		scores: [5, 1, 5, 4, 5, 5, 3, 5, 2, 1]
 	},
 	{
 		name: "Thor Odinson",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+		photo: "https://github.com/theworthyg/Friend-Finder/blob/master/app/friendpictures/thor.jpg?raw=true",
 		scores: [1, 5, 5, 5, 5, 4, 2, 1, 5, 1]
 	},
 	{
 		name: "Spider-Man",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+		photo: "https://github.com/theworthyg/Friend-Finder/blob/master/app/friendpictures/spiderman.jpg?raw=true",
 		scores: [5, 5, 2, 2, 5, 2, 4, 5, 1, 1]
 	},
 	{
 		name: "Hulk (Bruce Banner)",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		photo: "https://github.com/theworthyg/Friend-Finder/blob/master/app/friendpictures/Hulk_(comics_character).png?raw=true",
 		scores: [5, 5, 1, 1, 4, 3, 1, 2, 1, 5]
 	},
 	{
 		name: "Deadpool (Wade Wilson)",
-		photo: "",
+		photo: "https://github.com/theworthyg/Friend-Finder/blob/master/app/friendpictures/deadpool.jpg?raw=true",
 		scores: [5, 3, 5, 1, 5, 2, 2, 4, 3, 1]
     },
     {
 		name: "Groot",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		photo: "https://github.com/theworthyg/Friend-Finder/blob/master/app/friendpictures/groot.jpg?raw=true",
 		scores: [1, 3, 3, 1, 1, 5, 5, 1, 1, 5]
     },
     {
 		name: "Drax",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		photo: "https://github.com/theworthyg/Friend-Finder/blob/master/app/friendpictures/drax.png?raw=true",
 		scores: [1, 2, 4, 1, 1, 4, 2, 1, 3, 2]
     },
     {
 		name: "Black Widow (Natalia Rovenoff)",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		photo: "https://github.com/theworthyg/Friend-Finder/blob/master/app/friendpictures/blackwidow.jpg?raw=true",
 		scores: [5, 1, 4, 1, 2, 4, 3, 3, 2, 1]
     },
     {
 		name: "Stan Lee",
-		photo: "stanlee.png",
+		photo: "https://github.com/theworthyg/Friend-Finder/blob/master/app/friendpictures/stanlee.jpg?raw=true",
 		scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
 	},
     {
 		name: "Thanos",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		photo: "https://github.com/theworthyg/Friend-Finder/blob/master/app/friendpictures/thanos.jpg?raw=true",
 		scores: [1, 5, 5, 3, 2, 4, 1, 1, 5, 3]
 	}
     
